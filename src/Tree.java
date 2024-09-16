da8c1a9f3a7a692de2652f82518323d4c24674fd
amishmamtani <amishmamtani@gmail.com>
2024-09-16 18:41:47 -0400

    added Tree attributes

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,7 @@
+import org.w3c.dom.Node;
+
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    private int root;
+    private Tree[] subtrees;
 }
