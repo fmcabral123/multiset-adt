a0885e34406e88c4414b41c15a77dd68a0a00e51
issaccheung <issaccheung@outlook.com>
2024-09-16 18:59:51 -0400

    Implemented remove and contains

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,6 +1,7 @@
 public class TreeMultiSet extends MultiSet {
 
     // TODO add a Tree object as a private instance variable for this class.
+    private Tree tree;
     /**
      * Add the given item to this multiset.
      *
@@ -9,7 +10,14 @@ public class TreeMultiSet extends MultiSet {
     @Override
     void add(Integer item) {
         // TODO complete this method
+        this.tree.insert(item);
     }
     // TODO write the rest of the implementation for this class so that it uses its private Tree
     //      object to provide the MultiSet functionality.
+    void remove(Integer item) {
+        this.tree.delete_item(item);
+    }
+    boolean contains(Integer item) {
+        return
+    }
 }
