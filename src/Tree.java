7cffb2975e43f1addddb5e3880779793a9d2b0d6
amishmamtani <amishmamtani@gmail.com>
2024-09-16 18:50:12 -0400

    added Tree init

--- src/Tree.java
+++ src/Tree.java
@@ -4,4 +4,14 @@ public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
     private int root;
     private Tree[] subtrees;
+    public Tree(int root, Tree[] subtrees) {
+        this.root = root;
+        if (subtrees == null) {
+            Tree[] subs = {};
+            this.subtrees = subs;
+        }
+        else {
+            this.subtrees = subtrees;
+        }
+    }
 }
